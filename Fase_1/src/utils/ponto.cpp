08dbbfb8d0f7a77ec132ca81439e248d24b60799
Katilho <pemicama12@gmail.com>
2023-03-02 14:35:06 +0000

    completed cone, but it has a double free problem

--- Fase_1/src/utils/ponto.cpp
+++ Fase_1/src/utils/ponto.cpp
@@ -37,6 +37,21 @@ float getZ(Ponto p){
     return 0.0f;
 }
 
+float distanceToOrigin(Ponto p){
+    float dx = getX(p);
+    float dy = getY(p);
+    float dz = getZ(p);
+    return std::sqrt(dx*dx + dy*dy + dz*dz);
+}
+
+float distance(Ponto p1, Ponto p2){
+    float dx = getX(p2) - getX(p1);
+    float dy = getY(p2) - getY(p1);
+    float dz = getZ(p2) - getZ(p1);
+    return std::sqrt(dx*dx + dy*dy + dz*dz);
+}
+
+
 void deletePonto(Ponto p){
     free(p);
 }
--- Fase_1/src/utils/ponto.hpp
+++ Fase_1/src/utils/ponto.hpp
@@ -1,6 +1,7 @@
 #ifndef PONTO
 #define PONTO
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct ponto* Ponto;
 
@@ -13,6 +14,10 @@ float getX(Ponto p);
 float getY(Ponto p);
 
 float getZ(Ponto p);
+
+float distanceToOrigin(Ponto p);
+
+float distance(Ponto p1, Ponto p2);
 
 void deletePonto(Ponto p);
 
