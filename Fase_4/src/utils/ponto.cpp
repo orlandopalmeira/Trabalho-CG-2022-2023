2e3b40cbf3ff480f6580c5a41e093015a3f8fc73
orlandopalmeira <orlandopalmeira51@gmail.com>
2023-05-11 11:24:09 +0100

    Resolvido erro de argumento padrão na função `newPonto`

--- Fase_4/src/utils/ponto.cpp
+++ Fase_4/src/utils/ponto.cpp
@@ -8,7 +8,7 @@ Ponto newEmptyPonto(){
     return newPonto(0.0f, 0.0f, 0.0f);
 }
 
-Ponto newPonto(float x, float y, float z = 0.0f){
+Ponto newPonto(float x, float y, float z){
     Ponto r = (Ponto)malloc(sizeof(struct ponto));
     if(r != NULL){
         r->x = x; r->y = y; r->z = z;
